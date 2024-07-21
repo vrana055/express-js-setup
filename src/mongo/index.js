bd503e77c9dabc06598cfe25269391413e91100f
Vibhanshu Rana <vibhanshu@koyaltechsolutions.com>
2024-07-21 20:18:45 +0530

    add docker wtih app

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 export const dbConnect = () => {
-  mongoose.connect('mongodb+srv://mongodb000:Eg7ErFcCTYoqixwD@cluster0.ayur2dh.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0')
+  // mongoose.connect('mongodb+srv://mongodb000:Eg7ErFcCTYoqixwD@cluster0.ayur2dh.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0')
+  mongoose.connect('mongodb://mongodb-001:27017/sample_mflix?retryWrites=true&w=majority&appName=Cluster0')
   .then((obj) => {
     console.log('✨connected to mongodb');
     return Promise.resolve(obj);
